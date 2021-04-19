4e5bb5fd2f411cce44bb402e4e51753894c9d5b6
Alexei Kozhenko <kozhenko.a.v@gmail.com>
2021-04-20 00:42:03 +0300

    finish home-page/ finish section missin and start section team in about-page

--- app/js/main.js
+++ app/js/main.js
@@ -17,38 +17,50 @@ $ (function () {
     autoplaySpeed: 2500,
   });
 
-  mixitup('.product__items', {
-      selectors: {
-        control: '.product__button'
-      }
-  });
-  
-  mixitup('.new-design__products', {
-      selectors: {
-        control: '.new-design__btn'
-      }
+  const product__items = document.querySelector('.product__items')
+  if (product__items) {
+    // блок существует
+    mixitup('.product__items', {
+        selectors: {
+          control: '.product__button'
+        }
     });
+  }
 
-  $('.partner__list').slick({
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 650,
-        settings: {
-          slidesToShow: 2,
+  const design__products = document.querySelector('.new-design__products')
+  if (design__products) {
+    // блок существует
+    mixitup('.new-design__products', {
+        selectors: {
+          control: '.new-design__btn'
         }
-      },
-      {
-        breakpoint: 343,
-        settings: {
-          slidesToShow: 1,
+      });
+  }
+
+  const partner__list = document.querySelector('.partner__list')
+  if (partner__list) {
+    // блок существует
+    $('.partner__list').slick({
+      arrows: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 343,
+          settings: {
+            slidesToShow: 1,
+          }
         }
-      }
-    ]
-  });
+      ]
+    });
+  }
 
 });
